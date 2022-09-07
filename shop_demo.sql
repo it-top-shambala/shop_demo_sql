7c57eb16dc403169a6c8bc663317990b19fcef51
Starinin Andrey <starininandrey@gmail.com>
2022-09-07 20:20:24 +0300

    Update

--- shop_demo.sql
+++ shop_demo.sql
@@ -10,5 +10,11 @@ VALUES ('PC_1', 'Apple', 1000),
        ('PC_F', 'Asus', 100),
        ('Pad', 'Apple', 500),
        ('Pad new', 'HP', 230),
-       ('Phone', 'Apple', 2000);
+       ('Phone', 'Apple', 2000),
+       ('Phone', 'HP', 100);
 
+SELECT name, AVG(price)
+FROM table_products
+GROUP BY name;
+
+SELECT COUNT(*) FROM table_products WHERE manufacturer = 'Apple';
