431b110396068859d39673691f1104f2287c06a4
Starinin Andrey <starininandrey@gmail.com>
2022-09-07 20:47:26 +0300

    Update

--- shop_demo.sql
+++ shop_demo.sql
@@ -1,8 +1,9 @@
-CREATE TABLE table_products (
-    product_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name TEXT NOT NULL,
+CREATE TABLE table_products
+(
+    product_id   INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name         TEXT NOT NULL,
     manufacturer TEXT NOT NULL,
-    price INT NOT NULL DEFAULT 0
+    price        INT  NOT NULL DEFAULT 0
 );
 
 INSERT INTO shop_demo.table_products (name, manufacturer, price)
@@ -17,4 +18,22 @@ SELECT name, AVG(price)
 FROM table_products
 GROUP BY name;
 
-SELECT COUNT(*) FROM table_products WHERE manufacturer = 'Apple';
+SELECT COUNT(*)
+FROM table_products
+WHERE manufacturer = 'Apple';
+
+SELECT manufacturer
+FROM table_products
+WHERE price > 50
+GROUP BY manufacturer
+HAVING COUNT(*) > 1;
+
+DELIMITER |
+CREATE TRIGGER trigger_deny_delete_any
+    BEFORE DELETE
+    ON table_products
+    FOR EACH ROW
+BEGIN
+    SIGNAL SQLSTATE '90000';
+END |
+
